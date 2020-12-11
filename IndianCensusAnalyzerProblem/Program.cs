9d9abe57686fd3343a6bb1545bda7a2f97cd6e6d
“teji1998” <tejasvinirpk@gmail.com>
2020-12-11 23:56:38 +0530

    Add project files.

---
+++ IndianCensusAnalyzerProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianCensusAnalyzerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian Census Analyzer Problem !!!");
+        }
+    }
+}
